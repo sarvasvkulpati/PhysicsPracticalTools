e89f99694aecfc69b50b02cd73c37c79e7f71344
Rishabh Anand <mail.rishabh.anand@gmail.com>
2018-12-02 17:01:16 +0800

    Updated CSS and JS

--- script.js
+++ script.js
@@ -1,43 +1,78 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var maxGradient = document.getElementById('max-grad')
+    var minGradient = document.getElementById('min-grad')
+    var averageGradient = document.getElementById('avg-grad')
+    var units = document.getElementById('units')
 
+    var submit = document.getElementById('submit')
+    var outputArea = document.getElementById('output')
+    var answerArea = document.getElementById('answer')
 
-function calculate(){
-    var maxGradient = document.getElementsByName("max-gradient")[0].value;
-    var minGradient = document.getElementsByName("min-gradient")[0].value;
-    var averageGradient = document.getElementsByName("average-gradient")[0].value;
-    var units = document.getElementsByName("units")[0].value;
+    submit.onclick = function() {
+        console.log(maxGradient.value, minGradient.value, averageGradient.value, units.value)
+        if (maxGradient.value.length > 0 && minGradient.value.length > 0 && averageGradient.value.length > 0 && units.value.length > 0) {
+            var absoluteUncertainty = ((parseFloat(maxGradient.value) - parseFloat(minGradient.value))/2).toPrecision(4)
+            var percentageUncertainty = ((absoluteUncertainty/parseFloat(averageGradient.value))*100).toPrecision(4)
+            
+            var output = `
+                M<sub>avg</sub> = Average Gradient,<br>
+                M<sub>max</sub> = Maximum Gradient,<br> 
+                M<sub>min</sub> = Minimum Gradient<br>
+                <br>
+                M<sub>avg</sub> = ${averageGradient.value} ${units.value},<br>
+                M<sub>max</sub> = ${maxGradient.value}  ${units.value},<br> 
+                M<sub>min</sub> = ${minGradient.value}  ${units.value}<br>
+                <br>
+                <b>Gradient Uncertainty</b>
+                <br>
+                = (M<sub>max</sub> - M<sub>min</sub>)/2 <br>
+                = (${maxGradient.value} - ${minGradient.value})/2 <br>
+                = ${absoluteUncertainty} ${units.value}<br>
+                <br>
+                <b>Percentage Uncertainty</b>
+                <br>
+                = (Gradient Uncertainty / M<sub>avg</sub>) * 100 <br>
+                = (${absoluteUncertainty} / ${averageGradient.value}) * 100<br>
+                = ${percentageUncertainty}%<br>
+            `
 
-    if(maxGradient && minGradient && averageGradient && units){
-        var absoluteUncertainty = ((parseFloat(maxGradient) - parseFloat(minGradient))/2).toPrecision(4)
-        var percentageUncertainty = ((absoluteUncertainty/parseFloat(averageGradient))*100).toPrecision(4)
-        
-        
+            copyOutput = `
+                M_avg= Average Gradient,
+                M_max = Maximum Gradient,
+                M_min = Minimum Gradient
+                
+                M_avg = ${averageGradient.value} ${units.value},
+                M_max = ${maxGradient.value}  ${units.value},
+                M_min = ${minGradient.value}  ${units.value}
+                
+                Gradient Uncertainty
+                
+                = (M_max - M_min)/2
+                = (${maxGradient.value} - ${minGradient.value})/2
+                = ${absoluteUncertainty} ${units.value}
+                
+                Percentage Uncertainty
+                
+                = (Gradient Uncertainty / M_avg) * 100
+                = (${absoluteUncertainty} / ${averageGradient.value}) x 100
+                = ${percentageUncertainty}%
+            `
 
-        var output = `
-        M<sub>avg</sub> = gradient,<br>
-        M<sub>max</sub> = maximum gradient,<br> 
-        M<sub>min</sub> = minimum gradient<br>
-        <br>
-        M<sub>avg</sub> = ${averageGradient} ${units},<br>
-        M<sub>max</sub> = ${maxGradient}  ${units},<br> 
-        M<sub>min</sub> = ${minGradient}  ${units}<br>
-        <br>
-        <br>
-        <b>Gradient Uncertainty</b><br>
-        = (M<sub>max</sub> - M<sub>min</sub>)/2 <br>
-        = (${maxGradient} - ${minGradient})/2 <br>
-        = ${absoluteUncertainty} ${units}<br>
-        <br>
-        <b>Percentage Uncertainty</b><br>
-        = (Gradient Uncertainty / M<sub>avg</sub>) * 100 <br>
-        = (${absoluteUncertainty} / ${averageGradient}) * 100<br>
-        = ${percentageUncertainty}%<br>
-    
-       `
-        
-        
-        document.getElementById("output").innerHTML = output
-    } else {
-        alert("Please fill in all fields")
+            outputArea.style.display = 'block'
+            answerArea.innerHTML = output
+            answerArea.style.padding = '20px 0'
+
+            var copyButton = document.getElementById('copy-button')
+            copyButton.onclick = () => {
+                const el = document.createElement('textarea');
+                el.value = copyOutput;
+                document.body.appendChild(el);
+                el.select();
+                document.execCommand('copy');
+                document.body.removeChild(el);            
+            }
+        } else {
+            window.alert('Please fill in all fields!')
+        }
     }
-    
-}
+})
